03557d6cf9473fd587f9774ff2190adf072845ed
Beck <169068547+becklindo@users.noreply.github.com>
2024-09-03 01:08:20 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class Main {
+  public static void main(String[] args) {
+      System.out.println("#########");
+      System.out.println("série: cobra kai#");
+      System.out.println("#########");
+      System.out.println("Cobra Kai traz o retorno de Johnny Lawrence (William Zabka) e Daniel LaRusso (Ralph Macchio), os protagonistas de Karatê Kid, mais de 30 anos depois do clássico dos anos 80. Lawrence decide reabrir o dojo Cobra Kai, o que reacende a rivalidade dos dois inimigos de adolescência#");
+        System.out.println("https://www.youtube.com/watch?v=2QbtwDpKWxk#");
+        
+      
+      
+
+  
